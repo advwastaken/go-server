2a80ebbfe646adcf83792f86d4d57ccf2e656405
adv-wastaken <advwastaken@protonmail.com>
2022-06-18 11:48:53 +0530

    Updated

--- main.go
+++ main.go
@@ -6,9 +6,23 @@ import (
   "net/http"
 )
 
+func handleFormSubmit(w http.ResponseWriter, r *http.Request){
+  if err := r.ParseForm(); err != nil {
+    fmt.Fprintf(w, "error %v", err)
+    return
+  }
+  
+  fmt.Fprintf(w, "Post successful")
+  name := r.FormValue("name")
+  age := r.FormValue("age")
+  fmt.Fprintf(w, "Your name submitted as %s \n", name)
+  fmt.Fprintf(w, "Your age submitted as %d", age)
+}
+
 func main() {  
    Fileserver := http.FileServer(http.Dir("./static"))
    http.Handle("/", Fileserver)
+   http.HandleFunc("/form", handleFormSubmit)
    
    fmt.Printf("Starting server on port %d", 8080)
    if err := http.ListenAndServe(":8080", nil); err != nil {
